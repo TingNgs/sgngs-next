d1ca3bd582d28fc69b9dac32614df5294574f17a
TingNgs <t105590049@ntut.org.tw>
2019-11-22 01:17:56 +0800

    Test jenkins

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import Head from "next/head";
 import Nav from "../components/nav";
 
-const Home = () => <div>Updating v2</div>;
+const Home = () => <div>Updating v.2</div>;
 
 export default Home;
