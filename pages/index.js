93f3edd7d8bd1681a3418732813d9dd9edce5afd
TingNgs <t105590049@ntut.org.tw>
2019-11-22 01:10:27 +0800

    Add v2

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import Head from "next/head";
 import Nav from "../components/nav";
 
-const Home = () => <div>Updating</div>;
+const Home = () => <div>Updating v2</div>;
 
 export default Home;
